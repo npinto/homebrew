0bd2ccbf6f0ee642b4c04c5492194d1412192d55
Nicolas Pinto <pinto@mit.edu>
2011-10-02 14:18:15 -0400

    Add formula for tmux-1.4

---
+++ Library/Formula/tmux-1.4.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Tmux14 < Formula
+  url 'http://sourceforge.net/projects/tmux/files/tmux/tmux-1.4/tmux-1.4.tar.gz'
+  md5 '0bfc7dd9a5bab192406167589c716a21'
+  homepage 'http://tmux.sourceforge.net'
+
+  depends_on 'libevent'
+
+  def install
+    ENV['PREFIX'] = prefix
+    system "./configure"
+
+    inreplace "GNUmakefile" do |s|
+      # Put docs in the right place
+      s.gsub! "man/man1", "share/man/man1"
+    end
+
+    system "make install"
+  end
+end
